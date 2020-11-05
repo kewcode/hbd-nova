cf8c3fed0c4059db0a95185d807c02f1578da5d0
kewcode <aribahtiarpro@gmail.com>
2020-11-06 00:38:49 +0700

    app

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   mode: 'spa',
   pwa: {
     manifest: {
-      name: process.env.npm_package_name,
+      name: "HBD Nova",
       lang: 'id'
     }
   },
@@ -21,7 +21,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-     title: process.env.npm_package_name || '',
+     title: 'Selamat Ulang Tahun Nova Arista yang ke 21',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
